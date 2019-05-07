4e48b8ea2eda59f7c7d665482dcdb592a68012a5
maheshkumar3945@gmail.com <maheshkumar3945@gmail.com>
2019-05-07 17:38:56 -0400

    second commit

---
+++ demogradle/demogradle/src/main/java/com/example/demogradle/DemogradleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogradleApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemogradleApplication.class, args);
+		System.out.println("Hello gradle");
+	}
+
+}
